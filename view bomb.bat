c98c31c025067e276200fa9d428da4e437c6a8ef
Sap <69219781+5flow@users.noreply.github.com>
2020-09-25 08:53:09 -0400

    Add files via upload

---
+++ view bomb.bat
@@ -0,0 +1,51 @@
+@echo off
+
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+
+echo Choose your default browser you want the youtube video to be opened with.
+
+pause
+
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
+start "" https://www.youtube.com/watch?v=UBrxEa_9lbY&t=10s
